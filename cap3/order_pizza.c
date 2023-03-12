3b311df802a8544edd1858373c5df6c993ef44b7
Carlos <75546924+agapemakaveli@users.noreply.github.com>
2023-03-12 10:18:57 +0100

    Fixing the faul segmentation. Implementing getopt.

--- cap3/order_pizza.c
+++ cap3/order_pizza.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-#include <unistd.h>
+/*#include <unistd.h> */ // unistd.h does not declare optarg or optind as external, so fail under -std=99
+#include <getopt.h>
 
 int main(int argc, char *argv[])
 {
+
     char *delivery = "";
     int thick = 0;
     int ingredientCount = 0;
@@ -26,14 +28,14 @@ int main(int argc, char *argv[])
     argv += optind;
 
     if (thick)
-        puts("Thick crust.");
-  
+        puts("Thick crust");
+
     if (delivery[0])
-      
         printf("To be delivered %s.\n", delivery);
+
     puts("Ingredients:");
-  
-    for (int ingredientCount = 0; ingredientCount < argc; ingredientCount++)
+
+    for (ingredientCount = 0; ingredientCount < argc; ingredientCount++)
         puts(argv[ingredientCount]);
 
     return 0;
