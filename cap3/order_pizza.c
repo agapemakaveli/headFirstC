f82fb19b67feab72b9d0db93de583a66df3734c1
Carlos <75546924+agapemakaveli@users.noreply.github.com>
2023-03-12 10:02:35 +0100

    Create order_pizza.c

---
+++ cap3/order_pizza.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    char *delivery = "";
+    int thick = 0;
+    int ingredientCount = 0;
+    char ch;
+
+    while ((ch = getopt(argc, argv, "d:t")) != EOF)
+        switch (ch)
+        {
+        case 'd':
+            delivery = optarg;
+            break;
+        case 't':
+            thick = 1;
+            break;
+        default:
+            fprintf(stderr, "Unknown option: '%s\n", optarg);
+            return 1;
+        }
+
+    argc -= optind;
+    argv += optind;
+
+    if (thick)
+        puts("Thick crust.");
+  
+    if (delivery[0])
+      
+        printf("To be delivered %s.\n", delivery);
+    puts("Ingredients:");
+  
+    for (int ingredientCount = 0; ingredientCount < argc; ingredientCount++)
+        puts(argv[ingredientCount]);
+
+    return 0;
+}
